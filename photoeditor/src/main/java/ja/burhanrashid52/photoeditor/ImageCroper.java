a24bb106b3b3e90b03fb8c34cfeee07fbad67d20
Khawaja Qasim <khawaja.qasim@ilsainteractive.com>
2019-03-14 17:37:22 +0500

    fixed int crop image

--- photoeditor/src/main/java/ja/burhanrashid52/photoeditor/ImageCroper.java
+++ photoeditor/src/main/java/ja/burhanrashid52/photoeditor/ImageCroper.java
@@ -5,30 +5,19 @@ import android.content.Intent;
 import android.support.v4.app.Fragment;
 
 public class ImageCroper {
+
     public static final int CROP_IMAGE_RESULT = 100;
     public static final String EXTRA_CROP_IMAGE = "image_croper_image";
+
     private Activity activity;
     private Fragment fragment;
     private String path;
 
-    private ImageCroper(){
+    public ImageCroper(CropBuilder cropBuilder) {
+        this.activity = cropBuilder.activity;
+        this.fragment = cropBuilder.fragment;
+        this.path = cropBuilder.path;
 
-    }
-    public static ImageCroper build(String path, Activity activity){
-        ImageCroper imageCroper = new ImageCroper();
-        imageCroper.path = path;
-        imageCroper.activity = activity;
-        return imageCroper;
-    }
-
-    public static ImageCroper build(String path, Fragment fragment){
-        ImageCroper imageCroper = new ImageCroper();
-        imageCroper.path = path;
-        imageCroper.fragment = fragment;
-        return imageCroper;
-    }
-
-    public void start(){
         if (fragment != null) {
             Intent intent = new Intent(fragment.getContext(), CropImageActivity.class);
             intent.putExtra(EXTRA_CROP_IMAGE, path);
@@ -39,6 +28,27 @@ public class ImageCroper {
             intent.putExtra(EXTRA_CROP_IMAGE, path);
             activity.startActivityForResult(intent, CROP_IMAGE_RESULT);
         }
-
     }
+
+
+    public static class CropBuilder {
+        private Activity activity;
+        private Fragment fragment;
+        private String path;
+
+        public CropBuilder(String path, Activity activity) {
+            this.activity = activity;
+            this.path = path;
+        }
+
+        public CropBuilder(String path, Fragment fragment) {
+            this.fragment = fragment;
+            this.path = path;
+        }
+
+        public ImageCroper start() {
+            return new ImageCroper(this);
+        }
+    }
+
 }
