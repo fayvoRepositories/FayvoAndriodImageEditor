989a4e973934eba27b27264148c86645501371ff
fayvoRepositories <41581214+fayvoRepositories@users.noreply.github.com>
2020-03-12 21:36:42 +0500

    remove crop animation

--- photoeditor/src/main/java/ja/burhanrashid52/photoeditor/CropImageActivity.java
+++ photoeditor/src/main/java/ja/burhanrashid52/photoeditor/CropImageActivity.java
@@ -243,13 +243,13 @@ public class CropImageActivity extends AppCompatActivity implements View.OnClick
             }
 
             finish();
-            overridePendingTransition(R.anim.exit, R.anim.enter);
+            overridePendingTransition(0,0);
         } catch (Exception e) {
             Log.d("Exception", e.getStackTrace().toString());
             LocalMessageManager.getInstance().send(EXTRA_CROP_CANCEL);
             setResult(RESULT_CANCELED);
             finish();
-            overridePendingTransition(R.anim.exit, R.anim.enter);
+            overridePendingTransition(0,0);
         }
     }
 
